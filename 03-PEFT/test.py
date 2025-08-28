6a2adf8194e604f000f8f5f75ad8fde11c6beaec
朱幸文 <9888027+zhu-xingwen@user.noreply.gitee.com>
2025-08-28 21:11:08 +0800

    828

---
+++ 03-PEFT/test.py
@@ -0,0 +1,17 @@
+def process_func(example):
+    MAX_LENGTH = 256
+    input_ids, attention_mask, labels = [], [], []
+    instruction = tokenizer("\n".join(["Human: " + example["instruction"], example["input"]]).strip() + "\n\nAssistant: ")
+    response = tokenizer(example["output"] + tokenizer.eos_token)
+    input_ids = instruction["input_ids"] + response["input_ids"]
+    attention_mask = instruction["attention_mask"] + response["attention_mask"]
+    labels = [-100] * len(instruction["input_ids"]) + response["input_ids"]
+    if len(input_ids) > MAX_LENGTH:
+        input_ids = input_ids[:MAX_LENGTH]
+        attention_mask = attention_mask[:MAX_LENGTH]
+        labels = labels[:MAX_LENGTH]
+    return {
+        "input_ids": input_ids,
+        "attention_mask": attention_mask,
+        "labels": labels
+    }
